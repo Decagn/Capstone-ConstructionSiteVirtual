9c0a6f585ecc6f1b659ccb168743c516ed798895
Decagn <snewm@hotmail.com>
2026-04-17 01:09:28 +0930

    Setup array to hold different player prefabs

--- Assets/Scripts/Player/PlayerManager.cs
+++ Assets/Scripts/Player/PlayerManager.cs
@@ -1,17 +1,16 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+//This class is responsible for manager what 
 public class PlayerManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    //Array of player prefabs that can be spawned in the scene. This allows for easy switching between different player controllers (e.g. first person, third person, etc.) without changing code.
+    [Header("Player Prefabs")]
+    [SerializeField] private GameObject[] playerPrefabs;
 
-    // Update is called once per frame
-    void Update()
+    private GameObject currentPlayer;
+    private void Awake()
     {
-        
+        currentPlayer = Instantiate(playerPrefabs[0], transform.position, Quaternion.identity);
     }
 }
