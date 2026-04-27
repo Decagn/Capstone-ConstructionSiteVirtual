eb2f4864d2001710a91bec66bcbc92f033524831
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2026-04-27 11:24:41 +0930

    feat: dynamically load house FBX models on start

---
+++ Assets/Scripts/LoadModel.cs
@@ -0,0 +1,47 @@
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class LoadModel : MonoBehaviour
+{
+    GameObject[] houseModels;
+
+    void Start()
+    {
+        Camera mainCamera = Camera.main;
+
+        houseModels = Resources.LoadAll<GameObject>("House Models/");
+
+        // Display error message if no house models found
+        if (houseModels.Length == 0)
+        {
+            GameObject canvasObj = GameObject.Find("Canvas");
+
+            if (canvasObj != null)
+            {
+                Debug.Log("Error: no house models found in expected location");
+            }
+
+            return;
+        }
+
+        foreach (GameObject model in houseModels)
+        {
+            GameObject newModel = Instantiate<GameObject>(model, Vector3.zero, Quaternion.identity);
+            houseModels.Append(newModel);
+        }
+
+        // Disable all additional cameras within FBX models
+        Camera[] allCameras = Camera.allCameras;
+
+        if (allCameras != null)
+        {
+            foreach (Camera camera in allCameras)
+            {
+                camera.enabled = false;
+            }
+        }
+
+        mainCamera.enabled = true;
+    }
+}
