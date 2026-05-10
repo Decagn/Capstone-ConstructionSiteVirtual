c4fe1b32a0250f50015dbe25c362312a3f625af7
thivit <114897003+thivit@users.noreply.github.com>
2026-05-10 14:49:50 +0930

    Implement touch controls for mobile

--- Assets/NeshAssets/MeasuringTool/InputReading/InputListener.cs
+++ Assets/NeshAssets/MeasuringTool/InputReading/InputListener.cs
@@ -1,6 +1,8 @@
 using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class InputListener : MonoBehaviour
 {
@@ -10,36 +12,72 @@ public class InputListener : MonoBehaviour
     public event Action OnNextTool;
     public event Action OnPrevTool;
 
-    [SerializeField] private InputAction _selectPoint;
-    [SerializeField] private InputAction _deselectLastPoint;
-    [SerializeField] private InputAction _resetAllPoints;
-    [SerializeField] private InputAction _nextTool;
-    [SerializeField] private InputAction _prevTool;
+    private InputAction _selectPoint;
+    private InputAction _deselectLastPoint;
+    private InputAction _resetAllPoints;
+    private InputAction _nextTool;
+    private InputAction _prevTool;
 
     private void OnEnable()
     {
-        _selectPoint.Enable();
-        _deselectLastPoint.Enable();
-        _resetAllPoints.Enable();
-        _nextTool.Enable();
-        _prevTool.Enable();
+        EnableAllActions();
+        if (_isMobile) EnhancedTouchSupport.Enable();
     }
 
     private void OnDisable()
     {
-        _selectPoint.Disable();
-        _deselectLastPoint.Disable();
-        _resetAllPoints.Disable();
-        _nextTool.Disable();
-        _prevTool.Disable();
+        DisableAllActions();
+        if (_isMobile) EnhancedTouchSupport.Disable();
     }
 
     private void Awake()
     {
-        AddAllDefaultBindings();
+        SetupActions();
     }
 
     private void Update()
+    {
+        if (_isMobile)
+        {
+            HandleMobileInput();
+        }
+        else
+        {
+            HandleDesktopInput();
+        }
+    }
+    private void EnableAllActions()
+    {
+        _selectPoint?.Enable();
+        _deselectLastPoint?.Enable();
+        _resetAllPoints?.Enable();
+        _nextTool?.Enable();
+        _prevTool?.Enable();
+    }
+    private void DisableAllActions()
+    {
+        _selectPoint?.Disable();
+        _deselectLastPoint?.Disable();
+        _resetAllPoints?.Disable();
+        _nextTool?.Disable();
+        _prevTool?.Disable();
+    }
+    private void SetupActions()
+    {
+        _selectPoint = new InputAction("SelectPoint");
+        _deselectLastPoint = new InputAction("DeselectLastPoint");
+        _resetAllPoints = new InputAction("ResetAllPoints");
+        _nextTool = new InputAction("NextTool");
+        _prevTool = new InputAction("PrevTool");
+
+        if (!_isMobile)
+            BindDesktopKeys();
+
+        EnableAllActions();
+    }
+
+    #region Desktop
+    private void HandleDesktopInput()
     {
         if (_selectPoint.WasPressedThisFrame()) OnSelectPoint?.Invoke(GetClickPostion());
         else if (_deselectLastPoint.WasPressedThisFrame()) OnDeselectLastPoint?.Invoke();
@@ -47,19 +85,94 @@ public class InputListener : MonoBehaviour
         else if (_nextTool.WasPressedThisFrame()) OnNextTool?.Invoke();
         else if (_prevTool.WasPressedThisFrame()) OnPrevTool?.Invoke();
     }
-
     private Vector2 GetClickPostion() => Mouse.current.position.ReadValue();
-
-    private void AddDefaultBinding(InputAction action, string binding)
+    private void AddBinding(InputAction action, string binding)
     {
         if (action.bindings.Count == 0) action.AddBinding(binding);
     }
-    private void AddAllDefaultBindings()
+    private void BindDesktopKeys()
     {
-        AddDefaultBinding(_selectPoint, "<Mouse>/leftButton");
-        AddDefaultBinding(_deselectLastPoint, "<Mouse>/rightButton");
-        AddDefaultBinding(_resetAllPoints, "<Keyboard>/r");
-        AddDefaultBinding(_nextTool, "<Mouse>/scroll/up");
-        AddDefaultBinding(_prevTool, "<Mouse>/scroll/down");
+        AddBinding(_selectPoint, "<Mouse>/leftButton");
+        AddBinding(_deselectLastPoint, "<Mouse>/rightButton");
+        AddBinding(_resetAllPoints, "<Keyboard>/r");
+        AddBinding(_nextTool, "<Mouse>/scroll/up");
+        AddBinding(_prevTool, "<Mouse>/scroll/down");
     }
+    #endregion
+
+    #region Mobile
+    /*
+     * [MOBILE CONTROL SCHEME]
+     * Single tap: select point
+     * Hold: deselect last point
+     * Double tap: reset all points
+     * Two finger swipe up: switch to next tool
+     * Two finger swip donw: switch to prev tool
+     */
+    private bool _isMobile = Application.isMobilePlatform;
+    private Vector2 _touchStartPos;
+    private float _touchStartTime;
+    [SerializeField] private float _holdThreshold = 0.4f;
+    [SerializeField] private float _swipeThreshold = 50f;
+    private bool _holdFired;
+
+    private void HandleSingleTouchGestures(Touch touch)
+    {
+        if (touch.phase == UnityEngine.InputSystem.TouchPhase.Began)
+        {
+            _touchStartPos = touch.screenPosition;
+            _touchStartTime = Time.time;
+            _holdFired = false;
+        }
+
+        if (touch.phase == UnityEngine.InputSystem.TouchPhase.Stationary 
+            || touch.phase == UnityEngine.InputSystem.TouchPhase.Moved)
+        {
+            bool heldLongEnough = Time.time - _touchStartTime >= _holdThreshold;
+            bool notMovedMuch = Vector2.Distance(touch.screenPosition, _touchStartPos) < _swipeThreshold;
+
+            if (heldLongEnough && notMovedMuch && !_holdFired)
+            {
+                _holdFired = true;
+                OnDeselectLastPoint?.Invoke();
+            }
+        }
+
+        if (touch.phase == UnityEngine.InputSystem.TouchPhase.Ended)
+        {
+            bool wasQuickTap = Time.time - _touchStartTime < _holdThreshold;
+            bool notMovedMuch = Vector2.Distance(touch.screenPosition, _touchStartPos) < _swipeThreshold;
+
+            if (wasQuickTap && notMovedMuch && !_holdFired)
+            {
+                if (touch.tapCount == 2) OnResetAllPoints?.Invoke();
+                else OnSelectPoint?.Invoke(touch.screenPosition);
+            }
+        }
+    }
+    private void HandleTwoFingerGestures(Touch touchA, Touch touchB)
+    {
+        if (touchA.phase == UnityEngine.InputSystem.TouchPhase.Ended
+            || touchB.phase == UnityEngine.InputSystem.TouchPhase.Ended)
+        {
+            float deltaA = touchA.screenPosition.y - touchA.startScreenPosition.y;
+            float deltaB = touchB.screenPosition.y - touchB.startScreenPosition.y;
+            float averageDelta = (deltaA + deltaB) / 2f;
+
+            if (Mathf.Abs(averageDelta) > _swipeThreshold)
+            {
+                if (averageDelta > 0)
+                    OnNextTool?.Invoke();
+                else
+                    OnPrevTool?.Invoke();
+            }
+        }
+    }
+    private void HandleMobileInput()
+    {
+        var touches = Touch.activeTouches;
+        if (touches.Count == 1) HandleSingleTouchGestures(touches[0]);
+        else if (touches.Count == 2) HandleTwoFingerGestures(touches[0], touches[1]);
+    }
+    #endregion
 }
