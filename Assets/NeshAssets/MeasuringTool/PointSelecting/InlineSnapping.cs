76e52dad3479f5e85258450e15fecb55b9636cea
thivit <114897003+thivit@users.noreply.github.com>
2026-05-31 20:32:01 +0930

    document and clean up point selecting

--- Assets/NeshAssets/MeasuringTool/PointSelecting/InlineSnapping.cs
+++ Assets/NeshAssets/MeasuringTool/PointSelecting/InlineSnapping.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public static class InlineSnapping
 {
+    // Returns a list of points that are on the vertical line or horizontal plane of the other points in the scene.
     public static List<Vector3> GetPoints(Vector3 point, List<Vector3> otherPoints)
     {
         List<Vector3> inlinePoints = new List<Vector3>();
 
-        foreach (Vector3 otherP in otherPoints)
+        foreach (Vector3 otherPoint in otherPoints)
         {
-            inlinePoints.Add(new Vector3(otherP.x, point.y, otherP.z));
-            inlinePoints.Add(new Vector3(point.x, otherP.y, point.z));
+            inlinePoints.Add(new Vector3(otherPoint.x, point.y, otherPoint.z));
+            inlinePoints.Add(new Vector3(point.x, otherPoint.y, point.z));
         }
 
         return inlinePoints;
--- Assets/NeshAssets/MeasuringTool/PointSelecting/PointSelector.cs
+++ Assets/NeshAssets/MeasuringTool/PointSelecting/PointSelector.cs
@@ -4,144 +4,151 @@ using UnityEngine.InputSystem;
 
 public class PointSelector : MonoBehaviour
 {
-    [SerializeField] private Camera _camera;
+    // The player's camera to draw raycasts from.
+    [SerializeField] private Camera _playerCam; 
+
+    // How far in the scene points can be selected from.
     [SerializeField] private float _furthestPoint = Mathf.Infinity;
 
-    [SerializeField] private bool _snapping = true;
+    [SerializeField] public bool _snapping = true;
 
+    // Snapping to points placed in the lesson plan.
     [SerializeField] private LessonSnapManager _lessonSnapManager;
-    [SerializeField] private bool _snapToLessonPoint = true;
-    [SerializeField] private float _snapToLessonPointDistance = 0.5f;
+    [SerializeField] private bool _lessonPointSnapping = true;
+    [SerializeField] private float _snapToLessonPointDistance = 0.2f;
 
-    [SerializeField] private bool _snapToPreviousPoint = true;
-    [SerializeField] private float _snapToPreviousPointDistance = 0.5f;
+    // Snapping to points that have previously been placed in the scene.
+    [SerializeField] private bool _previousPointSnapping = true;
+    [SerializeField] private float _snapToPreviousPointDistance = 0.2f;
 
-    [SerializeField] private bool _snapToInlinePoint = true;
-    [SerializeField] private float _snapToInlinePointDistance = 0.5f;
+    // Snapping to the vertical line or horizontal plane of the other points in the scene.
+    [SerializeField] private bool _inlinePointSnapping = true;
+    [SerializeField] private float _snapToInlinePointDistance = 0.2f;
 
     [SerializeField] private MeasManager _measManager;
+
+    bool _platformIsMobile = Application.isMobilePlatform 
+        || UnityEngine.Device.Application.isMobilePlatform;
+
+    /*
+     * Preview Point
+     * 
+     * Shows the position of the point on screen before the user selects it.
+     * previewPoint is the small point in the center.
+     * previewGrid is the larger rotating sphere with a grid texture.
+     */
     [SerializeField] private bool previewPoint = true;
+    [SerializeField] private float _previewPointSize = 0.03f;
     [SerializeField] private GameObject _previewGridPrefab;
     [SerializeField] private GameObject _previewPointPrefab;
-    [SerializeField] private float _previewPointSize = 0.03f;
-    private GameObject _previewGrid;
+
     private float _previewGridSize;
     private float _gridRotationSpeed = 100f;
     private GameObject _previewPoint;
-    
-    private void Awake()
+    private GameObject _previewGrid;
+
+    // Dynamically scale the grid to the preview point size.
+    private void Awake() { _previewGridSize = _previewPointSize * 4f; }
+        
+    private void Update() { ShowPreviewPoint(); }
+
+    private Vector3 TryGetPoint(Vector2 screenPoint)
     {
-        _previewGridSize = _previewPointSize * 4f;
-    }
-    private void Update()
-    {
-        ShowPointOnScreen();
-    }
-    private void ShowPointOnScreen()
-    {
-        bool _isMobile = Application.isMobilePlatform || UnityEngine.Device.Application.isMobilePlatform;
-        Vector2 screenPoint = new Vector2();
-    
-        if (_isMobile)
-        {
-            screenPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
-        }
-        else
-        {
-            screenPoint = Mouse.current.position.ReadValue();
-        }
-
-        Vector3 pointInWorld = GetPoint(screenPoint, _measManager.GetSelectedPoints());
-
-        if (_previewGrid == null)
-        {
-            GameObject previewGrid = GameObject.Instantiate(_previewGridPrefab);
-            previewGrid.transform.position = pointInWorld;
-            Scaler.ScaleObject(_camera, previewGrid, _previewGridSize);
-            _previewGrid = previewGrid;
-        }
-        else
-        {
-            _previewGrid.transform.position = pointInWorld;
-            _previewGrid.transform.Rotate(Vector3.up * _gridRotationSpeed * Time.deltaTime);
-            Scaler.ScaleObject(_camera, _previewGrid, _previewGridSize);
-        }
-
-        if (_previewPoint == null)
-        {
-            GameObject previewPoint = GameObject.Instantiate(_previewPointPrefab);
-            previewPoint.transform.position = pointInWorld;
-            Scaler.ScaleObject(_camera, previewPoint, _previewPointSize);
-            _previewPoint = previewPoint;
-        }
-        else
-        {
-            _previewPoint.transform.position = pointInWorld;
-            Scaler.ScaleObject(_camera, _previewPoint, _previewPointSize);
-        }
-    }
-
-    public Vector3 GetPoint(Vector2 screenPoint, List<Vector3> selecPoints)
-    {
-        (Vector3 point, RaycastHit hit) = TryGetPoint(screenPoint);
-
-        if (!_snapping) return point;
-
-        Vector3 snapPoint = GetSnapPoint(point, selecPoints);
-        return snapPoint;
-    }
-
-    private (Vector3 point, RaycastHit ray) TryGetPoint(Vector2 screenPoint)
-    {
-        Ray ray = _camera.ScreenPointToRay(screenPoint);
+        Ray ray = _playerCam.ScreenPointToRay(screenPoint);
         Physics.Raycast(ray, out RaycastHit hit, _furthestPoint);
-        return (hit.point, hit);
+        return hit.point;
     }
 
-    private Vector3 GetSnapPoint(Vector3 point, List<Vector3> prevSelecPoints)
-    {
-        if (_snapToLessonPoint)
-        {
-            List<Vector3> LessonPoints = _lessonSnapManager.GetPoints();
-            Vector3 lessonPoint = FindClosestPoint(point, LessonPoints);
-            bool inLessonSnappingRange = Vector3.Distance(point, lessonPoint) < _snapToLessonPointDistance;
-            if (inLessonSnappingRange) return lessonPoint;
-        }
-
-        if (_snapToPreviousPoint && prevSelecPoints.Count > 0)
-        {
-            Vector3 prevPoint = FindClosestPoint(point, prevSelecPoints);
-            bool inPrevSnapRange = Vector3.Distance(prevPoint, point) < _snapToPreviousPointDistance;
-            if (inPrevSnapRange) return prevPoint;
-        }
-
-        if (_snapToInlinePoint)
-        {
-            List<Vector3> inlinePoints = InlineSnapping.GetPoints(point, prevSelecPoints);
-            Vector3 inlinePoint = FindClosestPoint(point, inlinePoints);
-            bool inInlineSnapRange = Vector3.Distance(inlinePoint, point) < _snapToInlinePointDistance;
-            if (inInlineSnapRange) return inlinePoint;
-        }
-
-        return point;
-    }
-
+    // Searches a list of vectors and returns the closest point.
     public static Vector3 FindClosestPoint(Vector3 point, List<Vector3> candidatePoints)
     {
-        Vector3 closest = point;
+        Vector3 closestPoint = point;
         float distance = Mathf.Infinity;
 
         foreach (Vector3 candidate in candidatePoints)
         {
             float candidateDistance = Vector3.Distance(candidate, point);
-            //Debug.Log($"Candidate point: {candidate} --> {candidateDistance}");
             if (candidateDistance < distance)
             {
                 distance = candidateDistance;
-                closest = candidate;
+                closestPoint = candidate;
             }
         }
 
-        return closest;
+        return closestPoint;
+    }
+    
+    private Vector3 GetPointToSnapTo(Vector3 point, List<Vector3> prevSelecPoints)
+    {
+        if (_lessonPointSnapping)
+        {
+            List<Vector3> LessonPoints = _lessonSnapManager.GetPoints();
+            Vector3 lessonPoint = FindClosestPoint(point, LessonPoints);
+            bool inLessonSnappingRange = Vector3.Distance(point, lessonPoint) < _snapToLessonPointDistance;
+            if (inLessonSnappingRange) 
+                return lessonPoint;
+        }
+
+        if (_previousPointSnapping && prevSelecPoints.Count > 0)
+        {
+            Vector3 prevPoint = FindClosestPoint(point, prevSelecPoints);
+            bool inPrevSnapRange = Vector3.Distance(prevPoint, point) < _snapToPreviousPointDistance;
+            if (inPrevSnapRange) 
+                return prevPoint;
+        }
+
+        if (_inlinePointSnapping)
+        {
+            List<Vector3> inlinePoints = InlineSnapping.GetPoints(point, prevSelecPoints);
+            Vector3 inlinePoint = FindClosestPoint(point, inlinePoints);
+            bool inInlineSnapRange = Vector3.Distance(inlinePoint, point) < _snapToInlinePointDistance;
+            if (inInlineSnapRange) 
+                return inlinePoint;
+        }
+
+        return point;
+    }
+
+    public Vector3 GetPoint(Vector2 screenPoint, List<Vector3> selecPoints)
+    {
+        Vector3 point = TryGetPoint(screenPoint);
+
+        if (!_snapping) 
+            return point;
+
+        Vector3 snapPoint = GetPointToSnapTo(point, selecPoints);
+        return snapPoint;
+    }
+
+    /*
+     * Preview Updates
+     * 
+     * Change the position and relative size of the preview each frame.
+     */
+    private void UpdatePreview(Vector3 pointInWorld)
+    {
+        _previewPoint.transform.position = pointInWorld;
+        Scaler.ScaleObject(_playerCam, _previewPoint, _previewPointSize);
+
+        _previewGrid.transform.position = pointInWorld;
+        _previewGrid.transform.Rotate(Vector3.up * _gridRotationSpeed * Time.deltaTime);
+        Scaler.ScaleObject(_playerCam, _previewGrid, _previewGridSize);
+    }
+
+    private void ShowPreviewPoint()
+    {
+        // On mobile, the screen point is just the middle of the screen.
+        Vector2 screenPoint = _platformIsMobile ? new Vector2(Screen.width / 2f, Screen.height / 2f) : Mouse.current.position.ReadValue();
+
+        // Use the same logic as point selecting so preview point would visually snap to snapping points.
+        Vector3 pointInWorld = GetPoint(screenPoint, _measManager.GetSelectedPoints());
+
+        if (_previewGrid == null)
+            _previewGrid = GameObject.Instantiate(_previewGridPrefab);
+
+        if (_previewPoint == null)
+            _previewPoint = GameObject.Instantiate(_previewPointPrefab);
+
+        UpdatePreview(pointInWorld);
     }
 }
