d5dd2abcebabbe23451b6ee626f3c2290c27b52c
thivit <114897003+thivit@users.noreply.github.com>
2026-06-03 19:16:13 +0930

    document and clean up scaling

--- Assets/NeshAssets/MeasuringTool/Scaling/Scaler.cs
+++ Assets/NeshAssets/MeasuringTool/Scaling/Scaler.cs
@@ -3,23 +3,30 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Rendering;
 
+/*
+ * Scaler
+ * This static class scales different classes of objects
+ * to appropriate sizes as the player/camera moves further or closer to those objects.
+ */
 public static class Scaler
 {
-    // returns a multiplier that scales the object depending on its distance from the player's camera.
+    // returns a multiplier that scales the object depending on its distance from the camera.
     private static float GetMultiplier(Camera cam, GameObject obj)
     {
         float distance = Vector3.Distance(cam.transform.position, obj.transform.position);
+
         // multiplier formula derived from quadratic regression using Desmos Graphing Calculator.
         double multiplier = -0.003f * Mathf.Pow(distance, 2) + 0.25 * distance - 0.15;
-        // clamp multiplier so it does not get too big / small
-        float clampedMult = Mathf.Clamp((float)multiplier, 0.5f, 2);
-        return clampedMult;
+
+        // clamp multiplier so its not too big / small
+        return Mathf.Clamp((float)multiplier, 0.5f, 2);
     }
 
     public static void ScaleObject(Camera cam, GameObject obj, float defaultSize)
     {
         float multiplier = GetMultiplier(cam, obj);
         float newSize = defaultSize * multiplier;
+
         obj.transform.localScale = new Vector3(newSize, newSize, newSize);
     }
 
@@ -38,6 +45,7 @@ public static class Scaler
         TextMeshProUGUI text = textObj.GetComponent<TextMeshProUGUI>();
         float multiplier = GetMultiplier(cam, textObj);
         float newFontSize = defaultFontSize * multiplier;
+
         text.fontSize = newFontSize;
     }
 }
