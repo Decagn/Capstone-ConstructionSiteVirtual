570d6186681dc5a370a060caa371bacdd08e7eaf
Silveda-dev <148528713+Silveda-dev@users.noreply.github.com>
2026-06-08 19:56:08 +0930

    feat: add instruction popup that can be toggled

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,43 @@
+/// <summary>
+/// Use the EasyPopup library to create a popup to provide basic instructions when the game is opened.
+/// </summary>
+
+using EasyPopupSystem;
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TutorialManager : MonoBehaviour
+{
+    public EasyPopup tutorialPopup;
+
+    // Create information popup upon game start
+    void Start()
+    {
+        CreateTutorialPopup();
+    }
+
+    void CreateTutorialPopup()
+    {
+        tutorialPopup = EasyPopup.Create("Welcome",
+            "Navigate through the house and complete the tasks in each room.",
+            "PopupInfo",
+            null,
+            null,
+            false,
+            "Press enter to continue"
+        );
+    }
+
+    // Toggle popup from keyboard input
+    void Update()
+    {
+        if (Keyboard.current.enterKey.wasPressedThisFrame && tutorialPopup != null)
+        {
+            tutorialPopup.Hide();
+        } else if (Keyboard.current.hKey.wasPressedThisFrame && tutorialPopup == null)
+        {
+            CreateTutorialPopup();
+        }
+    }
+}
