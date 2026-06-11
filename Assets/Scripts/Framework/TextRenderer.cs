f5ef79ee66d45cf804be8ecdc92c59137241b90b
thivit <114897003+thivit@users.noreply.github.com>
2026-06-11 20:50:25 +0930

    make rendered text face the player

--- Assets/Scripts/Framework/TextRenderer.cs
+++ Assets/Scripts/Framework/TextRenderer.cs
@@ -4,15 +4,26 @@
 
 using System.Collections.Generic;
 using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TextRenderer : MonoBehaviour
 {
+    // Get Camera for text alignment
+    [SerializeField] Camera _playerCamera;
     public List<GameObject> modelLabels;
 
     void Start()
     {
         modelLabels = new List<GameObject>();
+    }
+
+    private void Update()
+    {
+        // Make text face player
+        foreach(GameObject model in modelLabels)
+            model.transform.forward = _playerCamera.transform.forward;
     }
 
     public void RenderText(List<TextLabel> labelData)
@@ -25,12 +36,19 @@ public class TextRenderer : MonoBehaviour
         {
             Debug.Log($"Adding label '{label.text}' at position {label.labelPos}");
             GameObject newLabel = new GameObject($"Label: {label.text}");
-            newLabel.transform.position = label.labelPos;
 
-            TextMeshPro newText = newLabel.AddComponent<TextMeshPro>();
-            newText.transform.SetParent(newLabel.transform);
+            RectTransform rect = newLabel.AddComponent<RectTransform>();
+            rect.transform.position = label.labelPos;
+            rect.localScale = new Vector3(0.1f, 0.1f, 0.1f);
+
+            Canvas labelCanvas = newLabel.AddComponent<Canvas>();
+            labelCanvas.renderMode = RenderMode.WorldSpace;
+            labelCanvas.worldCamera = UnityEngine.Camera.main;
+
+            TextMeshProUGUI newText = labelCanvas.AddComponent<TextMeshProUGUI>();
             newText.text = label.text;
             newText.fontSize = label.fontSize;
+            newText.alignment = TextAlignmentOptions.Center;
             modelLabels.Add(newLabel);
         }    
     }
